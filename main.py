cb28faeef5f69aab382125e8d6afcda8a2c52c68
Utkarsh Gautam <111337138+utkarshgautam20@users.noreply.github.com>
2023-04-07 00:06:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-import os
-list1 = [1, 3, 5, 7, 10]
-list1.reverse()
-print("the reverse list is", list1)
-
-list2 = [91, 67, 85, 99, 44]
-list2.sort()
-print ("The sorted list is", list2)
-
-list3 = []
-list3= list1.copy()
-print("list3=",list3)
-indexvalue = list2[2:6]
-print("The index value are" , indexvalue)
+def calculate_sum(numbers):
+    return sum(numbers)
