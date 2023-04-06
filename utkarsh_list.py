fbf16e2f4133ca280b51efff0d597dc4ddd671c1
Utkarsh Gautam <111337138+utkarshgautam20@users.noreply.github.com>
2023-04-06 17:31:28 +0530

    Create utkarsh_list.py

---
+++ utkarsh_list.py
@@ -0,0 +1,14 @@
+import os
+list1 = [1, 3, 5, 7, 10]
+list1.reverse()
+print("the reverse list is", list)
+
+list2 = [91, 67, 85, 99, 44]
+list2.sort()
+print ("The sorted list is", list2)
+
+list3 = []
+list3= list1.copy()
+print("list3=",list3)
+indexvalue = list2[2:6]
+print("The index value are" , indexvalue)
